f7239dab34d0c68650c585f54a104251ddaf6d5d
SSOsh <tngh147258@naver.com>
2022-11-09 17:27:49 +0900

    Feat : CORS를 위한 annotation추가

--- src/main/java/shop/kokodo/apigatewayservice/ApigatewayServiceApplication.java
+++ src/main/java/shop/kokodo/apigatewayservice/ApigatewayServiceApplication.java
@@ -4,7 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 
 @SpringBootApplication
@@ -14,5 +18,15 @@ public class ApigatewayServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApigatewayServiceApplication.class, args);
     }
-
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 }
